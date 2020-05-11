f4f5cc284fd3df9c0b01b57c2a2084df6b899f37
Madley Sk <madleysk@gmail.com>
2020-05-11 10:27:28 -0400

    change db_url to heroku postgre

--- launch_flask_app.bat
+++ launch_flask_app.bat
@@ -1,5 +1,5 @@
 . venv/bin/activate
-export DATABASE_URL='sqlite:///mydb.sqlite3'
+export DATABASE_URL='postgres://uysuxskwgtdfdb:9b6cdbc17a2904a174df36c5e3fa3818f5c8061907b73fc8fb754d4592e79ab1@ec2-34-230-149-169.compute-1.amazonaws.com:5432/dc40p89r16sv6r'
 export SECRET_KEY=b'_9#y6K"G4Q0c\n\xec]/'
 export FLASK_APP=application.py
 export FLASK_ENV=development
