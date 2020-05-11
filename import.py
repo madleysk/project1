d7e1ee4fb1d72e89225e29ca1bd32cc4d621b6c8
Madley <madleysk@gmail.com>
2020-05-11 11:36:45 -0400

    change import script

--- import.py
+++ import.py
@@ -8,28 +8,26 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-conn = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode='require')
-
 def main():
 	# Creating tables structure if not exists
 	with open('db_sql.sql') as fichier:
 		lignes = fichier.read().split(';')
 		for ligne in lignes:
 			if len(ligne)<10:
-				conn.execute(ligne)
-				conn.commit()
-		conn.commit()
+				db.execute(ligne)
+				db.commit()
+		db.commit()
 	# inserting books
 	line_count = 0
 	with open("books.csv") as f:
 		reader = csv.reader(f)
 		for isbn, title, author, year  in reader:
 			if line_count > 0:
-				conn.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+				db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
 						   {"isbn": isbn, "title": title, "author": author, "year": year})
 				#print(f"Added book {title} from {author} appeared in {year}.")
 			line_count += 1
-		conn.commit()
+		db.commit()
 	print(f"{line_count-1} books imported !")
 
 """
